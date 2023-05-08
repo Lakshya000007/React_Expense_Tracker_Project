eb1aaecd97175c2bf0ee2cc0440a23f1f78c6481
Lakshya000007 <lakshya719@gmail.com>
2023-05-08 11:46:12 +0530

    Adding CSS and  JS in Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
+import "./ExpenseItem.css";
+
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 298.64;
+  const LocationOfExpenditure = "Banglore";
+
   return (
-    <>
-      <h3>Expense Item</h3>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
-    </>
+    <div className="expense-item">
+      <div>{expenseDate.toString()}</div>
+      <div className="expense-item__description">
+        <h2>
+          {expenseTitle} | Location :- {LocationOfExpenditure}
+        </h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
   );
 };
 
