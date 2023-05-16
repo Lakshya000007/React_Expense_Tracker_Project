a9017bbf850f648cc8dc308e514ccb76aa24a549
Lakshya000007 <lakshya719@gmail.com>
2023-05-16 10:41:23 +0530

    Add useSate to change Cost to 100$ onClick event

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./ExpenseDetails.css";
 
 const ExpenseDetails = ({ title, location, amount }) => {
+  const [cost, setCost] = useState(amount);
+
   const handleClick = (e) => {
     const parent = e.target.parentElement.parentElement.parentElement;
     const child = e.target.parentElement.parentElement;
@@ -9,13 +12,18 @@ const ExpenseDetails = ({ title, location, amount }) => {
     parent.removeChild(child);
   };
 
+  const handleCost = () => {
+    setCost(100);
+  };
+
   return (
     <>
       <div className="expense-item__description">
         <h2>
           {title} | Location : {location}
         </h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${cost}</div>
+        <button onClick={handleCost}>Change Expense to 100$</button>
         <button onClick={handleClick}>Delete Expense</button>
       </div>
     </>
