ca5fc9d43e5f91b6820ffe3e5a483cd01c36e651
Lakshya000007 <lakshya719@gmail.com>
2023-05-19 12:10:46 +0530

    Adding Filter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { useState } from "react";
 import ExpenseForm from "./components/AddExpense/ExpenseForm";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Card from "./components/UI/Card";
+import ExpensesFilter from "./components/Expenses/ExpensesFilter";
 
 function App() {
   const [expenses, setExpense] = useState([]);
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   const addExpenseHandler = (newExpense) => {
     setExpense([...expenses, newExpense]);
@@ -21,8 +28,12 @@ function App() {
       <ExpenseForm addExpenseHandler={addExpenseHandler} />
 
       <Card>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         <h1>
-          <center>App Component</center>
+          <center>Expenses List</center>
         </h1>
 
         {expenses.map((expense) => {
